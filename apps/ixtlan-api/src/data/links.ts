f5ae484aa29d8bf6f79164ab775f22a5299c73d8
AlanFnz <alan.fernandez@codeait.com>
2023-01-07 23:44:18 -0300

    added links data set

---
+++ apps/ixtlan-api/src/data/links.ts
@@ -0,0 +1,39 @@
+export interface Link {
+  id: string;
+  title: string;
+  value: string;
+  visible: boolean;
+  dateCreated: string;
+  lastUpdate: null;
+  type: string;
+}
+
+export const link: Link[] = [
+  {
+    id: '1',
+    title: 'bandcamp',
+    value: 'https://ixtlanmusic.bandcamp.com/',
+    visible: true,
+    dateCreated: '2023-01-08T02:35:06.060Z',
+    lastUpdate: null,
+    type: 'music',
+  },
+  {
+    id: '2',
+    title: 'twitter',
+    value: 'https://twitter.com/_ixtlan',
+    visible: true,
+    dateCreated: '2023-01-08T02:35:06.060Z',
+    lastUpdate: null,
+    type: 'social',
+  },
+  {
+    id: '3',
+    title: 'instagram',
+    value: 'https://instagram.com/ixtlan.io',
+    visible: true,
+    dateCreated: '2023-01-08T02:35:06.060Z',
+    lastUpdate: null,
+    type: 'social',
+  },
+];
