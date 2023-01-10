4e1cb8dd65bcab6644a72dc2d21446981de9d73b
AlanFnz <alan.fernandez@codeait.com>
2023-01-09 21:45:12 -0300

    cors

--- apps/ixtlan-api/src/main.ts
+++ apps/ixtlan-api/src/main.ts
@@ -1,15 +1,12 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
 import * as path from 'path';
+import * as cors from 'cors';
 
 // data set
 import { links } from './data/links';
 
 const app = express();
+app.use(cors());
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
